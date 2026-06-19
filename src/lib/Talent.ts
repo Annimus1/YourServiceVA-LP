004fe4184532c999088e828e024a42b2519b5164
Pablo Vergara <pablo.j98@gmail.com>
2026-06-19 12:31:58 -0400

    feat: Update Talent Section

---
+++ src/lib/Talent.ts
@@ -0,0 +1,42 @@
+export interface Talent {
+  name: string;
+  role: string;
+  image: string;
+  tags: string[];
+}
+
+const talents: Talent[] = [
+  {
+    name: "Steve Bravo",
+    role: "Bookkeeping Assistant",
+    image: "/img/agents/Steve.webp", // Asegúrate de colocar las imágenes en tu carpeta public
+    tags: ["QuickBooks", "Invoicing", "Accounts Payable", "Bank Reconciliation", "Financial Reports"]
+  },
+  {
+    name: "Keysha Thomas",
+    role: "Executive Virtual Assistant",
+    image: "/img/agents/Keysha.webp",
+    tags: ["Calendar Management", "Inbox Management", "Travel Coordination", "Client Communication", "CRM Tools"]
+  },
+  {
+    name: "William Narváez",
+    role: "Customer Support Specialist",
+    image: "/img/agents/william.webp",
+    tags: ["Email Support", "Live Chat", "Zendesk", "Troubleshooting", "Customer Retention"]
+  },
+  {
+    name: "Ana Castrillo",
+    role: "Executive Virtual Assistant",
+    image: "/img/agents/ana.webp",
+    tags: ["Calendar Management", "Inbox Management", "Travel Coordination", "Client Communication", "CRM Tools"]
+  },
+];
+
+export function getRandomTalent(amount:number): Talent[] {
+    return [];
+}
+
+export function getTalent() {
+    return talents;
+}
+
